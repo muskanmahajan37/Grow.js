6f02d60610d8be4db1759222672f2b3f9e0fa118
Gabriel Ferraz Duque <83476335+gabrielferrazduque@users.noreply.github.com>
2021-05-20 19:48:00 -0300

    Update and rename README.md to README.js

---
+++ README.js
@@ -0,0 +1,31 @@
+# Grow.js 
+while (true) { Grow.js}
+new Promise(function(resolve, reject) {
+  ClassName.prototype.methodName = function () {
+    // COMBAK: function* (Grow.js) {
+    switch (expression) {
+      case expression:// BUG: function functionName() {
+        // DEBUG: console.dir(Grow.js);
+      } getElementsByClassName('Grow.js')
+      case expression:
+
+        break;console.warn(Grow.js);
+
+    }
+      new Promise(function(resolve, reject) {
+        case expression:// BUG: function (Grow.js) {
+          // WARNING: switch (expression) {
+            case expression:do {
+
+            } while (true);  
+              while  (false); 
+              while(true);
+              while(function); 
+              class_name.prototype.method_name = function(first_argument) {
+              	// 
+              };
+      
+          break;Grow.js
+    
+return ();
+
